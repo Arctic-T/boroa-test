2952bc2dccbe5bcc0ff4ad93d91ceaaa0d74123e
Stefania Risso <teff.risso@gmail.com>
2023-01-22 20:42:22 -0300

    repository component styles

--- src/components/Repository.jsx
+++ src/components/Repository.jsx
@@ -1,5 +1,71 @@
+import RightArrow from "../icons/right-arrow";
+
 export default function Repository() {
   return (
-    <p>Repository</p>
-  )
+    <div className="repository">
+      <div className="repository-heading">
+        <p className="repository-title">Repositorio</p>
+        <p className="repository-subtitle">
+          Respositorio institucional de Boron Studio
+        </p>
+      </div>
+      <ul className="repository-list">
+        <li className="repository-item">
+          <a href="#" className="repository-item-link">
+            Autor
+            <img
+              src="/image.png"
+              className="repository-item-image"
+              alt="image"
+            />
+            <span className="repository-item-arrow">
+              <RightArrow />
+            </span>
+          </a>
+        </li>
+
+        <li className="repository-item">
+          <a href="#" className="repository-item-link">
+            Título
+            <img
+              src="/image.png"
+              className="repository-item-image"
+              alt="image"
+            />
+            <span className="repository-item-arrow">
+              <RightArrow />
+            </span>
+          </a>
+        </li>
+
+        <li className="repository-item">
+          <a href="#" className="repository-item-link">
+            Líneas de Navegación
+            <img
+              src="/image.png"
+              className="repository-item-image"
+              alt="image"
+            />
+            <span className="repository-item-arrow">
+              <RightArrow />
+            </span>
+          </a>
+        </li>
+
+        <li className="repository-item">
+          <a href="#" className="repository-item-link">
+            Descripción
+            <img
+              src="/image.png"
+              className="repository-item-image"
+              alt="image"
+            />
+            <span className="repository-item-arrow">
+              <RightArrow />
+            </span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
