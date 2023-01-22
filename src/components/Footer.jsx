badf8f9d7d9d1a670476223aeb68563d265e4eb6
Stefania Risso <teff.risso@gmail.com>
2023-01-22 18:16:43 -0300

    footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,34 +7,59 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <div className="footer-boron">
-          <div className="footer-boron-icon">
-            <Boron />
+        <div className="footer-boron-icon">
+          <Boron />
+        </div>
+        <div className="footer-links">
+          <div className="footer-links-group">
+            <a href="#" className="footer-links-link">
+              acerca de
+            </a>
+            <a href="#" className="footer-links-link">
+              proyectos
+            </a>
+            <a href="#" className="footer-links-link">
+              repositorio
+            </a>
+            <a href="#" className="footer-links-link">
+              novedades
+            </a>
           </div>
-          <p className="footer-news">enterate de las últimas novedades</p>
-          <div className="footer-information">
-            <button className="footer-information-button">
-              <span>Subscribite a nuestro newsletter</span> <RightArrow />
-            </button>
-            <p>info@coso</p>
-            <Youtube />
-            <Twitter />
+          <div className="footer-links-group">
+            <a href="#" className="footer-links-link">
+              contacto
+            </a>
+            <a href="#" className="footer-links-link">
+              lineas de investigación
+            </a>
+            <a href="#" className="footer-links-link">
+              información financiera
+            </a>
           </div>
         </div>
-        <div className="footer-list-wrapper">
-          <ul className="footer-list">
-            <li>acerca de</li>
-            <li>proyectos</li>
-            <li>repositorio</li>
-            <li>novedades</li>
-            <li>contacto</li>
-            <li>lineas de investigación</li>
-            <li>información financiera</li>
-          </ul>
+        <div className="footer-social">
+          <p className="footer-news">enterate de las últimas novedades</p>
+          <div className="footer-information">
+            <button className="footer-button">
+              <span className="footer-button-name">
+                Subscribite a nuestro newsletter
+              </span>
+              <RightArrow />
+            </button>
+            <div className="footer-social-network">
+              <p className="footer-mail">info@boronstudio.com</p>
+              <div className="footer-social-icons">
+                <Youtube />
+                <Twitter />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="footer-copyright">
-        <p>©2023 Todos los derechos reservados. Boron studio</p>
+        <p className="footer-copy">
+          ©2023 Todos los derechos reservados. Boron studio
+        </p>
         <p className="footer-by">bY boron studio</p>
       </div>
     </footer>
