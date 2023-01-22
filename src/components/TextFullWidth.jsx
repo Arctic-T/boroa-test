0141196c6d286925e23ceebb6a39d59a7d3f2ecb
Stefania Risso <teff.risso@gmail.com>
2023-01-22 19:08:06 -0300

    text full width styles

--- src/components/TextFullWidth.jsx
+++ src/components/TextFullWidth.jsx
@@ -1,5 +1,9 @@
 export default function TextFullWidth() {
   return (
-    <p>TextFullWidth</p>
-  )
+    <div className="text-full-width">
+      Somos una organización autónoma que busca resolver desafíos relacionados
+      con el aprendizaje y la mediación de las tecnologías, a través de
+      metodologías innovadoras y un abordaje práctico, social y participativo.
+    </div>
+  );
 }
