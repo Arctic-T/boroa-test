0b2265cb9af000e874734fe96162e8c3bfd39e01
Stefania Risso <teff.risso@gmail.com>
2023-01-24 11:09:51 -0300

    news effects updated

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -4,47 +4,62 @@ export default function News() {
       <h1 className="news-title">Novedades</h1>
       <div className="news-box-container">
         <div className="news-box">
-          <figure>
+          <figure className="news-box-figure">
             <img src="/news.png" className="news-box-image" alt="image" />
           </figure>
-          <div className="news-box-heading">
-            <p className="news-box-title">WEBINAR</p>
-            <span className="news-box-plus">+</span>
+          <div className="news-box-wrapper">
+            <div className="news-box-heading">
+              <p className="news-box-title">WEBINAR</p>
+              <span className="news-box-plus">+</span>
+            </div>
+            <p className="news-box-description">
+              Boron Studio invita a webinar sobre educación híbrida
+            </p>
           </div>
-          <p className="news-box-description">
-            Boron Studio invita a webinar sobre educación híbrida
-          </p>
         </div>
         <div className="news-box second">
-          <img src="/news.png" className="news-box-image" alt="image" />
-          <div className="news-box-heading">
-            <p className="news-box-title">EVENTOS</p>
-            <span className="news-box-plus">+</span>
+          <figure className="news-box-figure">
+            <img src="/news2.png" className="news-box-image" alt="image" />
+          </figure>
+          <div className="news-box-wrapper">
+            <div className="news-box-heading">
+              <p className="news-box-title">EVENTOS</p>
+              <span className="news-box-plus">+</span>
+            </div>
+            <p className="news-box-description">
+              Boron Studio invita a webinar sobre educación híbrida
+            </p>
           </div>
-          <p className="news-box-description">
-            Boron Studio invita a webinar sobre educación híbrida
-          </p>
         </div>
         <div className="news-box third">
-          <img src="/news.png" className="news-box-image" alt="image" />
-          <div className="news-box-heading">
-            <p className="news-box-title">WEBINAR</p>
-            <span className="news-box-plus">+</span>
+          <figure className="news-box-figure">
+            <img src="/news3.png" className="news-box-image" alt="image" />
+          </figure>
+          <div className="news-box-wrapper">
+            <div className="news-box-heading">
+              <p className="news-box-title">WEBINAR</p>
+              <span className="news-box-plus">+</span>
+            </div>
+            <p className="news-box-description">
+              Boron Studio presenta experiencia de educación a distancia en
+              México
+            </p>
           </div>
-          <p className="news-box-description">
-            Boron Studio presenta experiencia de educación a distancia en México
-          </p>
         </div>
         <div className="news-box fourth">
-          <img src="/news.png" className="news-box-image" alt="image" />
-          <div className="news-box-heading">
-            <p className="news-box-title">WEBINAR</p>
-            <span className="news-box-plus">+</span>
+          <figure className="news-box-figure">
+            <img src="/news4.png" className="news-box-image" alt="image" />
+          </figure>
+          <div className="news-box-wrapper">
+            <div className="news-box-heading">
+              <p className="news-box-title">WEBINAR</p>
+              <span className="news-box-plus">+</span>
+            </div>
+            <p className="news-box-description">
+              Publicación del libro “Tecnología: Lo que puede y no puede hacer
+              por la educación”
+            </p>
           </div>
-          <p className="news-box-description">
-            Publicación del libro “Tecnología: Lo que puede y no puede hacer por
-            la educación”
-          </p>
         </div>
       </div>
     </div>
